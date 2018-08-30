a8fb15eae73a3cba3d03471e28aae87f287d0f14
amashhour1974 <30394454+amashhour1974@users.noreply.github.com>
2018-08-30 22:45:40 +0200

    aws server sttatus

---
+++ aws_serverstatus_monitor.sh
@@ -0,0 +1,10 @@
+aws ec2 describe-instance-status --instance-ids i-0a5fb1ca6b0294f45 --region us-east-2
+
+Output
+
+INSTANCESTATUSES	us-east-2b	i-0a5fb1ca6b0294f45
+INSTANCESTATE	16	running
+INSTANCESTATUS	ok
+DETAILS	reachability	passed
+SYSTEMSTATUS	ok
+DETAILS	reachability	passed
